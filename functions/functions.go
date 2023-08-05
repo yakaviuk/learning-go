5f013d0f60b4ed0d3c7cbb65d00116ebf6befc91
Ilya Yakaviuk <yakavil@gmail.com>
2023-08-05 23:06:51 +0300

    functions

---
+++ functions/functions.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	myprint()
+	myprint()
+	myprint()
+
+}
+
+func myprint() {
+	fmt.Println("Print line from custom function")
+}
--- hello-world.go
+++ hello-world.go
@@ -4,20 +4,6 @@ import "fmt"
 
 func main() {
 
-	var a = "initial"
-	fmt.Println(a)
+	fmt.Println("Hello world")
 
-	var b, c int = 1, 2
-	fmt.Println(b, c)
-
-	var d = true
-	fmt.Println(d)
-
-	var e int
-	fmt.Println(e)
-
-	f := "apple"
-	fmt.Println(f)
-	fmt.Println("#######")
-	// fmt.Println("//////////////////////")
 }
