328081cefcaf1dfee91d8e0283bd2de5c8c5ae30
Ilya Yakaviuk <yakavil@gmail.com>
2023-08-05 23:31:24 +0300

    functions

--- functions/functions.go
+++ functions/functions.go
@@ -3,12 +3,25 @@ package main
 import "fmt"
 
 func main() {
-	myprint()
-	myprint()
-	myprint()
+	printMessage("foo")
+	printMessage("bar")
+	printMessage("baz")
 
+	mickHello := sayHello("Mick")
+	printMessage(mickHello)
+
+	printMessage(sayMore("Mick", 42))
 }
 
-func myprint() {
-	fmt.Println("Print line from custom function")
+func printMessage(message string) {
+	fmt.Println(message)
+}
+
+func sayHello(name string) string {
+	return "Hello, " + name
+}
+
+func sayMore(name string, age int) string {
+	return fmt.Sprintf("Hello, %s. You are %d years old.", name, age)
+	// return "Hello, " + name + ". You are " + age + " years old."
 }
