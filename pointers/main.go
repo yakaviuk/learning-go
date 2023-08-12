e95525fb894e3f7fcdba74b894b75e94a30a49f3
Ilya Yakaviuk <yakavil@gmail.com>
2023-08-12 12:59:32 +0300

    pointers

---
+++ pointers/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	message := "I will become Ninja soon"
+	printMsg(message)
+
+	fmt.Println(message)
+}
+
+func printMsg(message string) {
+	message += " (from printMsg() function)"
+	fmt.Println(message)
+}
