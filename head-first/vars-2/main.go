b4f8e52c42ed230e61e01284b7169d5fcce9597f
Ilya Yakaviuk <yakavil@gmail.com>
2023-08-26 19:35:19 +0300

    vars

---
+++ head-first/vars-2/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	another_name := "Lorenso Dalvatone"
+	length1, width1 := 5.1, 8.01
+	quantity1 := 4
+
+
+	fmt.Println(another_name, "has ordered", quantity1, "places.\nArea of the each place is", length1*width1, "square meters")
+}
--- head-first/vars/main.go
+++ head-first/vars/main.go
@@ -25,5 +25,4 @@ func main() {
 	quantity1 := 4
 
 	fmt.Println(another_name, "has ordered", quantity1, "places.\nArea of the each place is", length1*width1, "square meters")
-
 }
