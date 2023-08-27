03ef42a5bcf3e4d2202efd813498695fee9151d2
Ilya Yakaviuk <yakavil@gmail.com>
2023-08-27 19:49:32 +0300

    add strconv.ParseFloat

--- head-first/pass_fail/main.go
+++ head-first/pass_fail/main.go
@@ -6,6 +6,8 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
@@ -15,5 +17,18 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(input)
+
+	input = strings.TrimSpace(input)
+	grade, err := strconv.ParseFloat(input, 64)
+	if err != nil {
+		log.Fatal(err)
+	}
+	
+	var status string
+	if grade >= 60 {
+		status = "passed"
+	} else {
+		status = "failed"
+	}
+	fmt.Println(status)
 }
