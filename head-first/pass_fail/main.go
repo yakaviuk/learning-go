792d4ce34ff498fa981eb79627523fae7e9abc8f
Ilya Yakaviuk <yakavil@gmail.com>
2023-08-27 18:10:16 +0300

    add if to pass_fail

--- head-first/pass_fail/main.go
+++ head-first/pass_fail/main.go
@@ -12,6 +12,8 @@ func main() {
 	fmt.Print("Enter a grade: ")
 	reader := bufio.NewReader(os.Stdin)
 	input, err := reader.ReadString('\n')
-	log.Fatal(err)
+	if err != nil {
+		log.Fatal(err)
+	}
 	fmt.Println(input)
 }
